0c15c42c22a1259fdb8b8d5d9747ddea0b9c8155
Paolo Oliverio <paolo.oliverio.dev@gmail.com>
2025-10-16 10:45:28 +0200

    initial commit

---
+++ main/src/kis_test_package_b.cpp
@@ -0,0 +1,12 @@
+#include "../include/kis_test_package_b/kis_test_package_b.hpp"
+#include <iostream>
+
+namespace kis{
+namespace test_package_b{
+
+void hello() {
+    std::cout << "Hello from kis_test_package_b!" << std::endl;
+}
+
+}
+}
---
+++ secondary/tests/test_package_b.test.cpp
@@ -0,0 +1,8 @@
+#include <kis_test_package_b/kis_test_package_b.hpp>
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+TEST_CASE("Fake test that always passes") {
+    CHECK(true);
+}
