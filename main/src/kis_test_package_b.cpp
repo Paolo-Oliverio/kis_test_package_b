8ebe519dee82543ff8b1c24265315310d87b61cf
Paolo Oliverio <paolo.oliverio.dev@gmail.com>
2025-10-16 12:36:34 +0200

    add kis_test_package_a dependency for testing purpose.

--- main/src/kis_test_package_b.cpp
+++ main/src/kis_test_package_b.cpp
@@ -1,10 +1,12 @@
 #include "../include/kis_test_package_b/kis_test_package_b.hpp"
+#include <kis_test_package_a/kis_test_package_a.hpp>
 #include <iostream>
 
 namespace kis{
 namespace test_package_b{
 
 void hello() {
+    test_package_a::hello();
     std::cout << "Hello from kis_test_package_b!" << std::endl;
 }
 
